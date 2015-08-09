7d91ccc00a4bff13a3b32a124166e8443fa6da50
LMoros <LMoros1930@yahoo.com>
2015-08-09 14:42:50 -0400

    Adding .gitignore and cleaning up StandardPipeline

--- Pipelines/StandardPipeline.cs
+++ Pipelines/StandardPipeline.cs
@@ -8,10 +8,19 @@ namespace Pipelines
     {
         #region Contructor and Private Members
         private readonly ILog<string, string> _Logging;
-        private readonly IAuthenticate<T> _Authentication;
-        private readonly IAuthorize<T> _Authorization;
-        private readonly IProcess<T> _Process;
-        private readonly ITranslateToWebApi<IAmProcessed<IAmAuthorized<IAmAuthenticated<T>>>, R> _AdaptOutput;
+        private readonly Func<T, IAmAuthenticated<T>> _Authenticate;
+        private readonly Func<IAmAuthenticated<T>,
+                                IAmAuthorized<
+                                    IAmAuthenticated<T>>> _Authorize;
+        private readonly Func<IAmAuthorized<IAmAuthenticated<T>>,
+                                IAmProcessed<
+                                    IAmAuthorized<
+                                        IAmAuthenticated<T>>>> _Process;
+
+        private readonly ITranslateToWebApi<
+                            IAmProcessed<
+                                IAmAuthorized<
+                                    IAmAuthenticated<T>>>, R> _AdaptOutput;
 
         public StandardPipeline(
             ILog<string, string> logging,
@@ -21,30 +30,21 @@ namespace Pipelines
             ITranslateToWebApi<IAmProcessed<IAmAuthorized<IAmAuthenticated<T>>>, R> adapter) 
         {
             _Logging = logging;
-            _Authentication = authentication;
-            _Authorization = authorization;
-            _Process = process;
+            _Authenticate = authentication.Authenticate;
+            _Authorize = authorization.Authorize;
+            _Process = process.Process;
             _AdaptOutput = adapter;
         }
         #endregion
 
         public Task<R> Execute(T intension)
         {
-            #region definning local variables
-            Func<T, IAmAuthenticated<T>> authenticate = _Authentication.Authenticate;
-            Func<IAmAuthenticated<T>, 
-                 IAmAuthorized<IAmAuthenticated<T>>> authorize = _Authorization.Authorize;
-            Func<IAmAuthorized<IAmAuthenticated<T>>,
-                 IAmProcessed<IAmAuthorized<IAmAuthenticated<T>>>> process = _Process.Process;
-            var adaptOuput = _AdaptOutput;
-            #endregion
-
             return SetPipelineAs
                     .With( intension)
-                    .Asynchrosnully( authenticate.Including( Logging))
-                    .AndThen( authorize.Including( Logging).Retrying( up_to: 2.Times()))
-                    .AndThen( process.Including( Logging).Retrying( up_to: 5.Times()))
-                    .AtLast( adaptOuput);
+                    .Asynchrosnully(_Authenticate.Including( Logging))
+                    .AndThen(_Authorize.Including( Logging).Retrying( up_to: 2.Times()))
+                    .AndThen(_Process.Including( Logging).Retrying( up_to: 5.Times()))
+                    .AtLast(_AdaptOutput);
         }
 
         #region Logging overloads
